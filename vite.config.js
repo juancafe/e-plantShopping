7f27481a40319f42107961eeb0ad00548e05be41
Juan Carlos Ferriolo <jcfd@adinet.com.uy>
2024-09-10 16:09:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  plugins: [react()],
   base: "/e-plantShopping",
+  plugins: [react()],
+
   
 })
