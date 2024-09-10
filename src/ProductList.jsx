bad4b87feab1bee62addbbd79bc72419732e3bdc
juancafe <jcfd@adinet.com.uy>
2024-09-10 10:30:50 -0400

    message3

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -275,10 +275,10 @@ const handlePlantsClick = (e) => {
                 <div className="product-list">
                 {category.plants.map((plant, plantIndex) => (
                     <div className="product-card" key={plantIndex}>
-                      <img className="product-image" src={plant.image} alt={plant.name} />
-                    <div className="product-title">{plant.name}</div>
-                       {/*Similarly like the above plant.name show other details like description and cost*/}
-                      <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        <img className="product-image" src={plant.image} alt={plant.name} />
+                        <div className="product-title">{plant.name}</div>
+                         {/*Similarly like the above plant.name show other details like description and cost*/}
+                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                      </div>
                      ))}
                  </div>
